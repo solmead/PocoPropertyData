bf30b0ab5bb6f050f182fe6db31b6af105512cc5
Chris Powell <powelcr@ucmail.uc.edu>
2014-01-13 12:13:49 -0500

    Initial

---
+++ PocoPropertyData/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PocoPropertyData")> 
+<Assembly: AssemblyDescription("Poco Property Extender")> 
+<Assembly: AssemblyCompany("Solmead Productions")> 
+<Assembly: AssemblyProduct("PocoPropertyData")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("Solmead Productions")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("21d848b9-610f-4215-8393-3d2fc3f13060")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ PocoPropertyData/PackNuget.bat
@@ -0,0 +1,1 @@
+"..\.nuget\nuget" pack PocoPropertyData.vbproj -IncludeReferencedProjects
